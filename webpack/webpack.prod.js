e38ee4b79418ef2e93ed6be085237982f3d399c2
leizhang <lei.zhang.cn.nz@gmail.com>
2019-08-18 23:10:20 +1200

    react analyzer and down grade antd

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -5,15 +5,36 @@ const TerserJSPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 module.exports = merge(common, {
   mode: 'production',
   optimization: {
     minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    splitChunks: {
+      chunks: 'all',
+      cacheGroups: {
+        common: {
+          chunks: 'initial',
+        },
+        'antd': {
+          test: /antd/,
+          chunks: 'initial',
+          priority: 2
+        }
+      }
+    }
   },
   plugins: [
     new MiniCssExtractPlugin({
       filename: '[name].css',
       chunkFilename: '[id].css',
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'disable',
+      generateStatsFile: true,
+      analyzerPort: 3123,
+      statsOptions: { source: false },
     }),
   ]
 });
